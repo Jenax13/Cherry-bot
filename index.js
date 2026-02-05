7639684f9519b4bfa960393a536d026e21f1a6f2
Jenax13 <fundacionscp40@gmail.com>
2026-02-05 09:37:28 -0500

    Index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
+    console.log('Escanea el QR para iniciar Cherry bot 🍒');
+});
+
+client.on('ready', () => {
+    console.log('🍒 Cherry bot está lista 🍒');
+});
+
+client.on('message', message => {
+    const msg = message.body.toLowerCase();
+
+    if (msg === 'hola') {
+        message.reply('Hola~ 🍒 Soy Cherry bot, tu bot favorita');
+    } 
+    else if (msg.includes('cereza')) {
+        message.reply('🍒 Las cerezas son lindas, dulces y peligrosamente adictivas~');
+    }
+    else if (msg === '!menu') {
+        message.reply(
+`🍒 *Cherry bot menú* 🍒
+🌸 !hola
+🌸 !cereza
+🌸 !menu`
+        );
+    }
+});
+
+client.initialize();
